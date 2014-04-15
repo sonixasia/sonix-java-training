f96fe7c48b29e117e6f0b5f6e1103fb19016399a
kwakata <kwakata@sonix.asia>
2014-04-15 15:44:01 +0900

    Add question

---
+++ src/asia/sonix/training/java/Training_15_MultiplicationTable.java
@@ -0,0 +1,16 @@
+package asia.sonix.training.java;
+
+import java.io.IOException;
+
+public class Training_15_MultiplicationTable {
+
+    /**
+     * Training 15
+     * 
+     * 九九を出力
+     * 
+     */
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
